d23878a1ab5fb1db2808fc99a178d51b2c56955d
Naz <152059367+nazmulaminkhan@users.noreply.github.com>
2023-12-29 11:52:17 +0100

    Create arraylist.java

---
+++ Java Observable codes/arraylist.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class arraylist {
+    public static void main(String[] args){
+        //ArrayList = a resizable array.
+        //              Elements can be added and removed after compilation phase
+        //              store reference data types
+
+        ArrayList<String> food = new ArrayList<String>();
+        food.add("Pizza");
+        food.add("Hamburger");
+        food.add("Hot dog");
+        food.set(0, "KungPao");
+        food.remove(2);
+        food.clear();
+        for (int i=0;i<food.size();i++){
+            System.out.println(food.get(i));
+        }
+    }
+}
