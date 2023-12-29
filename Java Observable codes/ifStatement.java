9342b7d09328fda599cd7d3324fc98e6b44d9762
Naz <152059367+nazmulaminkhan@users.noreply.github.com>
2023-12-29 11:41:14 +0100

    Create ifStatement.java

---
+++ Java Observable codes/ifStatement.java
@@ -0,0 +1,18 @@
+public class ifStatement {
+    public static void main(String[]args){
+        // if statements = performs a block of code if it's condition evaluates to be true
+        int age = 14;
+        if(age>=75){
+            System.out.println("Ok, Boomer!");
+        }
+        else if(age>=18){
+            System.out.println("You are an adult");
+        }
+        else if(age>=13){
+            System.out.println("You are a teenager");
+        }
+        else{
+            System.out.println("You are not an adult");
+        }
+    }
+}
