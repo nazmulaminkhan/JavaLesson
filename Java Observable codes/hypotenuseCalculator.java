9a855786c07e5daed11ae744c7430ff948e6e4a6
Naz <152059367+nazmulaminkhan@users.noreply.github.com>
2023-12-29 11:39:13 +0100

    Update hypotenuseCalculator.java

--- Java Observable codes/hypotenuseCalculator.java
+++ Java Observable codes/hypotenuseCalculator.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+public class hypotenuse {
+    public static void main(String[]args){
+        double x;
+        double y;
+        double z;
 
+        Scanner hypos = new Scanner(System.in);
+        System.out.println("Enter the side of A: ");
+        x = hypos.nextDouble();
+        System.out.println("Enter the side of B: ");
+        y = hypos.nextDouble();
+        z=Math.sqrt((x*x)+(y*y));
+        System.out.println("The hypotenuse is: "+z);
+
+        hypos.close();
+
+    }
+}
