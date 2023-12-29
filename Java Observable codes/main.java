d790ac848e0b3d6d1962ba74ca546a8a26bdcd18
Naz <152059367+nazmulaminkhan@users.noreply.github.com>
2023-12-29 11:39:55 +0100

    Update main.java

--- Java Observable codes/main.java
+++ Java Observable codes/main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
-public class Main{
+public class main{
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.println("I like pizza!");
